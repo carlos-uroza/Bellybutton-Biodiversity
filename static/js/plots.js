863d9003180c95f40744b240f520c699ba2aaffc
carlos-uroza <carlos.uroza05@gmail.com>
2022-07-17 19:00:02 -0500

    upload

--- static/js/plots.js
+++ static/js/plots.js
@@ -2,7 +2,7 @@
 function init() {
     var selector = d3.select("#selDataset");
   
-    d3.json("samples.json").then((data) => {
+    d3.json("static/data/samples.json").then((data) => {
       console.log(data);
       var sampleNames = data.names;
       sampleNames.forEach((sample) => {
@@ -23,7 +23,7 @@ function init() {
 
   // Create function to print information to the Demographic Info panel
   function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
+    d3.json("static/data/samples.json").then((data) => {
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
